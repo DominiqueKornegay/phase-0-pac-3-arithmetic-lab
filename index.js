647c228fff915af4a8dc1e25873ab4cee280e5d9
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-04-27 10:56:38 -0400

    commit tree df0ffe0f8c61d91ceb1c3dfb2f66927076331ddd

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a ,b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function increment(n) {
+    return n + 1
+}
+
+function decrement(n) {
+    return n - 1
+}
+
+function makeInt(a, b) {
+    return parseInt(a,b)
+}
+
+function makeInt(a, b) {
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a, b) {
+    return parseFloat(a, b)
+}
